3e85b52f2fee8139c72e3295e93513b068d550fc
Jobin <jobin.s@cloudifyops.com>
2024-11-17 18:59:34 +0530

    updated ai prompt

--- ai_error_analysis.py
+++ ai_error_analysis.py
@@ -16,13 +16,13 @@ log_file.close()
 message = client.messages.create(
     model="anthropic.claude-v2",
     max_tokens=256,
-    messages=[{"role": "user", "content": f"Summary the error and fix for the following jenkins log:\n{log}"}]
+    messages=[{"role": "user", "content": f"Summary the error and fix for the following jenkins log:\n{log}\n Give output in html format. Provide only html report."}]
 )
 
 response = message.content[0].text
 output = re.sub(r"\n\n+", "\n", response)
 
-analysis_file = open('ai_analysis.html', 'a')
-analysis_file.write("<h1>AI Error Analysis</h1><br>")
+analysis_file = open('ai_analysis.html', 'w')
+# analysis_file.write("<h1>AI Error Analysis</h1><br>")
 analysis_file.write(output)
 analysis_file.close() 
